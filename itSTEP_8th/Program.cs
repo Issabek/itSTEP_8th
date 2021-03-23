6ae1728d5fe90003d61df5e4fe69b4583bac5ae4
Issabek <180107287@stu.sdu.edu.kz>
2021-03-23 13:42:29 +0600

    we

---
+++ itSTEP_8th/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using itSTEP_8th;
+
+namespace itSTEP_8th
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            tempArray arr = new tempArray(2);
+            arr[0] = 5;
+            arr[1] = 2;
+            Console.WriteLine(arr[0]+" "+arr[1]);
+            
+        }
+    }
+}
---
+++ itSTEP_8th/tempArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace itSTEP_8th
+{
+    public class tempArray
+    {
+        int[] temps;
+        public int Length => temps.Length;
+
+        public int this[int index]
+        {
+            get => temps[index];
+            set => temps[index] = value*value;
+        }
+        public tempArray(int size)
+        {
+            temps = new int[size];
+        }
+    }
+}
